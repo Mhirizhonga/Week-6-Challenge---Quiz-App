25072ffe1d9214f8ded27ca17450f4dbe8d00031
KodaKing <m.maso@live.co.uk>
2024-03-08 15:51:21 +0000

    Changed code in function startQuiz to add questionDiv/

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,25 +6,29 @@ document.addEventListener ("DOMContentLoaded", function ()  {
     const timeDisplay = document.getElementById("time");
     const finalScoreDisplay = document.getElementById("final-score");
     const initialsInput = document.getElementById("initials");
+    const submitButton = document.getElementById("submit");
+    const feedbackDiv = document.getElementById("feedback");
+    const choicesDiv = document.getElementById("choices");
 
 let currentQuestionIndex = 0;
-let timeLeft = 120;
+let timeLeft = 60;
+let timerInterval = null;
 
 const questions = [
     {
         question: "What is the capital of Zimbabwe?",
-        options: ["Cape Town", "Harare", "Accra", "Luanda"],
-        answer: "Harare"
+        choices: ["Cape Town", "Harare", "Accra", "Luanda"],
+        correctAnswer: "Harare"
     },
        {
         question: "Which planet is known as the Red Planet?",
-        options: ["Jupiter", "Mars", "Venus", "Saturn"],
-        answer: "Mars"
+        choices: ["Jupiter", "Mars", "Venus", "Saturn"],
+        correctAnswer: "Mars"
     },
        {
         question: "Who wrote 'Things Fall Apart'?",
-        options: ["Chimamanda Ngozi", "Tsitsi Dambarembga", "Chinua Achebe", "Aaron Chiundura Moyo"],
-        answer: "Chinua Achebe"
+        choices: ["Chimamanda Ngozi", "Tsitsi Dambarembga", "Chinua Achebe", "Aaron Chiundura Moyo"],
+        correctAnswer: "Chinua Achebe"
     },
     {
         question: "What is JavaScript?",
@@ -39,23 +43,83 @@ const questions = [
 ]
 
 function startQuiz()  {
+    //console.log(startQuiz);
     startButton.classList.add("hide");
-    questionsDiv.classList.add("hide");
+    //questionsDiv.classList.add("hide");
+    questionsDiv.removeAttribute("class");
     showQuestion();
     startTimer();
 }
 
 function showQuestion()  {
-    const currentQuestion = question[currentQuestionIndex];
-    document.getElementById("question-title").textContent = currentQuestion.question;
+    console.log(showQuestion);
+    const currentQuestion = questions[currentQuestionIndex];
+    console.log(currentQuestion);
+    const titleValue = document.getElementById("question-title")
+    titleValue.textContent = currentQuestion.question;
     choicesDiv.innerHTML = "";
-    currentQuestion.choices.forEach(choice =>  {
+    currentQuestion.choices.forEach( function (choice) {
+        console.log(choice);
         const button = document.createElement("button");
+        button.setAttribute("class", "choice")
+        button.setAttribute("value", choice)
         button.textContent = choice;
-        button.addEventListener("click", function ()  {
-            checkAnswer(choice === currentQuestion.correctAnswer);
-        });
+        // button.addEventListener("click", function ()  {
+        //     checkAnswer(choice === currentQuestion.correctAnswer);
+        // });
+        button.onclick = checkAnswer;
+        choicesDiv.appendChild(button);
     });
-}};
+}
 
-)
+function checkAnswer(isCorrect)  {
+    if (!isCorrect) {
+        timeLeft -= 10;
+        //console.log("10 seconds penalised!");
+        if (timeLeft < 0) {
+            timeLeft = 0;
+        }    
+    }
+    updateTimerDisplay();
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length)  {
+        showQuestion()
+    } else  {
+        endQuiz();
+    }
+}
+
+function startTimer()  {
+    const timerInterval = setInterval(function ()  {
+        timeLeft--;
+        updateTimerDisplay();
+        if (timeLeft === 0)  {
+            clearInterval(timerInterval);
+            endQuiz();
+        }
+    }, 1000);
+}
+
+function updateTimerDisplay()  {
+    timeDisplay.textContent = timeLeft;
+}
+
+function endQuiz()  {
+    clearInterval(timerInterval);
+    questionsDiv.classList.add("hide");
+    endScreenDiv.classList.remove("hide");
+    finalScoreDisplay.textContent = timeLeft;
+}
+
+submitButton.addEventListener("click", function ()  {
+
+    const initials = initialsInput.value.trim();
+    if (initials !== "") {
+        feedbackDiv.textContent = "Score saved!";
+    } else  {
+        feedbackDiv.textContent = "Please enter your initials!"
+    }
+});
+
+startButton.addEventListener("click", startQuiz);
+});
